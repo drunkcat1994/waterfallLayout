7927ae25e56d6088b089ef33398ca0955916398f
drunkcat1994 <767797706@qq.com>
2016-09-07 16:51:32 +0800

    change in 9.7

--- waterfall.js
+++ waterfall.js
@@ -1,5 +1,25 @@
 var data = {
 	href:[
+		'1.jpg',
+		'2.jpg',
+		'3.jpg',
+		'4.jpg',
+		'5.jpg',
+		'6.jpg',
+		'7.jpg',
+		'8.jpg',
+		'9.jpg',
+		'10.jpg',
+		'11.jpg',
+		'12.jpg',
+		'13.jpg',
+		'14.jpg',
+		'15.jpeg',
+		'16.jpg',
+		'17.jpg',
+		'18.jpg',
+		'19.jpg',
+		'20.jpg',
 		'21.jpg',
 		'22.jpg',
 		'23.jpg',
@@ -9,7 +29,17 @@ var data = {
 		'27.jpg',
 		'28.jpg',
 		'29.jpg',
-		'30.jpg'
+		'30.jpg',
+		'31.jpg',
+		'32.jpg',
+		'33.jpg',
+		'34.jpg',
+		'35.jpeg',
+		'36.jpg',
+		'37.jpg',
+		'38.jpg',
+		'39.jpg',
+		'40.jpg'
 	]
 };
 function getEleByClass(parent,name){
@@ -24,77 +54,74 @@ function getEleByClass(parent,name){
 }
 function getIndex(array,elem) {
 	for( var i in array) {
-		if(array[i] === elem ) {
+		if(array[i] == elem ) {
 			return i;
 		}
 	}
 }
 //判断是否加载
-function whetherOnload(array) {
-	var lastElem = array[array.length - 1];
+function checkScrollslide() {
+	var mainbox = document.getElementById('main');
+	var boxs = getEleByClass(mainbox,'box');
+	var lastElem = boxs[boxs.length - 1];
 	//取得浏览器的滚动高度和浏览器的窗口高度，以及最后一个元素距离页面顶端的top
-	var scrollHeight = document.body.scrollTop || document.bodyElement.scrollTop;
-	var clientHeight = document.body.clientHeight || document.bodyElement.clientHeight;
+	var scrollHeight = document.body.scrollTop || document.documentElement.scrollTop;
+	var clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
 	var wholeHeight = scrollHeight + clientHeight;
-	var topValue = lastElem.style.top;
-	console.log(topValue);
+	var topValue = lastElem.offsetTop;
 	if(topValue < wholeHeight) {
 		return true; 
 	} else {
 		return false;
 	}
 }
+function createBox(boxParent,data) {
+	var newBox = document.createElement('div');
+	newBox.className = 'box';
+	boxParent.appendChild(newBox);
+	var newImg = document.createElement('img');
+	newImg.src = 'images/' + data;
+	newBox.appendChild(newImg);
+}
 function waterfall() {
-	//获取main下的所有box
+	var sumTopValue = []
 	var mainbox = document.getElementById('main');
 	var boxs = getEleByClass(mainbox,'box');
-	var len = boxs.length;
-	//console.log(len);
-	//获取浏览器窗口宽度和box的宽度，计算显示的列数
-	//clientwidth是实时动态的宽度
+	//var len = boxs.length;
+	//取得单个box的宽度和浏览器的宽度，求得一行有多少值
+	var singleBoxW = boxs[0].offsetWidth;
 	var windowWidth = document.body.clientWidth || document.bodyElement.clientWidth;
-	var boxWidth = boxs[0].offsetWidth;
-	var rowNumber = Math.floor(windowWidth / boxWidth);
-	//console.log(rowNumber);
-	var mainWidth = Math.floor(rowNumber * boxWidth);
-	mainbox.style.width = mainWidth + 'px';
-	//用一个数组firstRow存放一行的box
-	var firstRowH = [];
-	for(var i =0; i < rowNumber;i++) {
-		var width = i * boxWidth;
-		boxs[i].style.cssText = 'position:absolute;';
-		boxs[i].style.cssText ='top:0;right:' + width +'px;';
-		firstRowH.push(boxs[i].offsetHeight);
+	var	rowNumber = Math.floor(windowWidth / singleBoxW);
+	var mainboxW = Math.floor(rowNumber * singleBoxW);
+	mainbox.style.width = mainboxW + 'px';
+	for(var i = 0;i < boxs.length; i++) {
+		if(i < rowNumber) {
+			sumTopValue.push(boxs[i].offsetHeight);//第一行不参与定位
+		} else {
+			var minH = Math.min.apply(null,sumTopValue);
+			var minindex = getIndex(sumTopValue,minH);
+			//var rightValue = index * singleBoxW;//第一次为0
+			var rightValue = boxs[minindex].offsetLeft;
+			boxs[i].style.position ='absolute';//设置绝对位移
+	        boxs[i].style.top = minH + 'px';
+	        boxs[i].style.left = rightValue + 'px';
+			sumTopValue[minindex] += boxs[i].offsetHeight;
+		}
+		
 	}
-	
-	//将下一个元素的position设置为最小高度的元素的top和left
-	for(var i = rowNumber;i < boxs.length;i++) {
-		var minH = Math.min.apply(null,firstRowH);
-		//console.log(minH);
-		var index = getIndex(firstRowH,minH);
-		console.log(index);
-		var rightWidth = index * boxWidth;
-		boxs[i].style.cssText = 'position:absolute;';
-		boxs[i].style.cssText ="top:" + minH + 'px;right:' + rightWidth +'px;';
-		firstRowH[index] = Math.floor(boxs[i].offsetHeight + minH);	
-		console.log(firstRowH[index]);
-	}
-		//每重新放置一次，就改变存放fistRow内元素的值
-	//写一个函数getIndex来获取数组内特定元素的下标
-	for(var i = 0; i < data.href.length;i++) {
-		if(whetherOnload(boxs)) {
-			var newBox = document.createElement('div');
-			newBox.className = 'box';
-			mainbox.appendChild(newBox);
-			var newImg = document.createElement('img');
-			newImg.className = data.href[i];
-			newBox.appendChild(newImg);
+
+
+}
+window.onload = function() {
+	waterfall();
+	window.onscroll=function(){
+		if(checkScrollslide()) {
+			var mainbox = document.getElementById('main');
+			for(var i = 0; i < data.href.length;i++) {
+				createBox(mainbox,data.href[i]);
+			}
+			waterfall(); 
 		}
 	}
 	
-}
-window.onload = function() {
-	//var x = Math.min.apply(null,n);
-	//console.log(x);
-	waterfall();
 }
